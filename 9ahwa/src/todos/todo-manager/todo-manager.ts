b9d70a833d3c27a1971176c5c26f6f0eec8ff16f
Bechir-Brahem <bachbrahem@gmail.com>
2022-04-25 01:55:40 +0100

    added more database examples

--- 9ahwa/src/todos/todo-manager/todo-manager.ts
+++ 9ahwa/src/todos/todo-manager/todo-manager.ts
@@ -5,6 +5,9 @@ import { CreateTodoDto } from '../dto/create-todo.dto';
 import { ConfigService } from '@nestjs/config';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { TodoStatusEnum } from '../enums/todo-status.enum';
+import { FindAllDto } from '../dto/find-all.dto';
+import { GroupedTodos } from '../classes/grouped-todos.class';
 
 @Injectable()
 export class ManageTodoService {
@@ -19,8 +22,34 @@ export class ManageTodoService {
     return this.todoRepository.save(todo);
   }
 
-  async findAll(): Promise<Todo[]> {
-    return await this.todoRepository.find();
+  async findAll(queries: FindAllDto): Promise<Todo[]> {
+    const { limit, offset, status, criteria } = queries;
+    const paginationOptions = { take: limit, skip: offset };
+
+
+    if (criteria && status) {
+      const queryBuilder = this.todoRepository.createQueryBuilder();
+      const query = queryBuilder
+        .select('todo')
+        .from(Todo, 'todo')
+        .where('todo.name = :criteria', { criteria: criteria })
+        .orWhere('todo.description = :criteria', { criteria: criteria })
+        .andWhere('todo.status = :status', { status: status })
+        .take(limit)
+        .skip(offset);
+      return query.getMany();
+
+    } else if (criteria)
+      return await this.todoRepository.find({
+        where: [
+          { name: criteria }, { description: criteria },
+        ],
+        ...paginationOptions,
+      });
+    else if (status)
+      return await this.todoRepository.find({ where: { status: status }, ...paginationOptions });
+    else
+      return await this.todoRepository.find(paginationOptions);
   }
 
   async findOne(id: number): Promise<Todo> {
@@ -45,6 +74,26 @@ export class ManageTodoService {
   async delete(id: number): Promise<Todo> {
     const todo = await this.findOne(id);
     return this.todoRepository.remove(todo);
+  }
+
+  async count(status: TodoStatusEnum): Promise<number> {
+    if (status)
+      return await this.todoRepository.countBy({ status: status });
+    else
+      return await this.todoRepository.count();
+  }
+
+  async countTodosGrouped() :Promise<GroupedTodos>{
+    const queryBuilder = this.todoRepository.createQueryBuilder();
+    const query = queryBuilder
+      .select('status, count(status) as count')
+      .groupBy('status');
+    const result=await query.getRawMany();
+    let ret= new GroupedTodos();
+    for(let elt of result){
+      ret[elt.status]=parseInt(elt.count,10);
+    }
+    return ret;
 
 
   }
--- 9ahwa/src/todos/todos.controller.ts
+++ 9ahwa/src/todos/todos.controller.ts
@@ -1,36 +1,51 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post } from "@nestjs/common";
-import {Todo} from "./entities/todo.entity"
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Todo } from './entities/todo.entity';
 import { ManageTodoService } from './todo-manager/todo-manager';
-import { CreateTodoDto } from "./dto/create-todo.dto";
-import { UpdateTodoDto } from "./dto/update-todo.dto";
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { FindAllDto } from './dto/find-all.dto';
+import { CountByStatusDto } from './dto/count-by-status.dto';
+import { GroupedTodos } from './classes/grouped-todos.class';
 
-@Controller("todo")
+@Controller('todo')
 export class TodosController {
   constructor(private readonly manageTodos: ManageTodoService) {
   }
 
   @Get()
-  getTodos(): Promise<Todo[]> {
-    return this.manageTodos.findAll();
+  getTodos(@Query() queries: FindAllDto): Promise<Todo[]> {
+    return this.manageTodos.findAll(queries);
   }
 
-  @Get(":id")
-  getTodo(@Param("id") id: number): Promise<Todo>{
-    return  this.manageTodos.findOne(id);
+  @Get(':id([0-9]+)')
+  getTodo(@Param('id') id: number): Promise<Todo> {
+    return this.manageTodos.findOne(id);
   }
 
   @Post()
-  addTodo(@Body() newTodo: CreateTodoDto): Promise<Todo>{
+  addTodo(@Body() newTodo: CreateTodoDto): Promise<Todo> {
     return this.manageTodos.addTodo(newTodo);
   }
 
-  @Patch(":id")
-  updateTodo(@Body() cuteTodo: UpdateTodoDto, @Param("id") id: number): Promise<Todo> {
+  @Patch(':id([0-9]+)')
+  updateTodo(@Body() cuteTodo: UpdateTodoDto, @Param('id') id: number): Promise<Todo> {
     return this.manageTodos.update(id, cuteTodo);
   }
 
-  @Delete(":id")
-  deleteTodo(@Param("id") id: number): Promise<Todo> {
+  @Delete(':id([0-9]+)')
+  deleteTodo(@Param('id') id: number): Promise<Todo> {
     return this.manageTodos.delete(id);
   }
+
+  @Get('count')
+  countTodos(@Query() queries: CountByStatusDto): Promise<number> {
+    const { status } = queries;
+    return this.manageTodos.count(status);
+  }
+
+  @Get('count/grouped/')
+  countTodosGrouped() : Promise<GroupedTodos> {
+    return this.manageTodos.countTodosGrouped();
+  }
+
 }
